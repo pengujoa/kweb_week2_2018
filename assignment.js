a13613b0011c3ca9700c5a5515facb60d5189d44
unknown <ckddyd0806@gmail.com>
2018-10-12 13:54:02 +0900

    Third step

--- assignment.js
+++ assignment.js
@@ -1,29 +1,23 @@
 const fs = require('fs');
+const util = require('util')
+const fs_readFile = util.promisify(fs.readFile)
+const fs_writeFile = util.promisify(fs.writeFile)
 
-function readFile(fileName,type){
-    return new Promise((resolve, reject)=>{
-        fs.readFile(fileName,type,(err,data)=> {
-            err ? reject(err) : resolve(data);
-        });
-    }); 
+async function homework(){
+    try{
+        await fs_writeFile('./1.txt', '11111');
+       await console.log('1.txt created');
+        const data1 = await fs_readFile('./1.txt','UTF-8');
+        await fs_writeFile('./2.txt', data1 + '22222');
+       await console.log('2.txt created');
+       const data2 = await fs_readFile('./2.txt','UTF-8');
+       await fs_writeFile('./3.txt', data2 + '33333');
+       await console.log('3.txt created');
+       const data3 = await fs_readFile('./3.txt','UTF-8');
+       await console.log(data3)
+    } catch(err){
+        console.log(err);
+    }
 }
 
-function writeFile(fileName,fow){
-    return new Promise((resolve, reject)=>{
-        fs.writeFile(fileName,fow,(err)=> {
-            err ? reject(err) : resolve(fow);
-        });
-    }); 
-}
-
-
-writeFile('./1.txt', '11111')
-    .then(function(){console.log('1.txt created'); return readFile('./1.txt','UTF-8');})
-    .then(function(data1){return writeFile('./2.txt',data1 + '22222');})
-    .then(function(){console.log('2.txt created'); return readFile('./2.txt','UTF-8');})
-    .then(function(data2){return writeFile('./3.txt',data2 + '33333');})
-    .then(function(){console.log('3.txt created'); return readFile('./3.txt','UTF-8');})
-    .then(function(data3){console.log(data3)})
-    .catch(function(err){
-    console.log(err);
-    });
+homework();
